96f45b14c3b1e53c934682509bd3ac7e91753115
Gele-CSharp <georgiev.angel@yahoo.com>
2021-08-22 13:18:57 +0300

    Added new solutions.

---
+++ C#Fundamentals/Integer Operations/Integer Operations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int forthNumber = int.Parse(Console.ReadLine());
+
+            if (thirdNumber != 0)
+            {
+                Console.WriteLine(forthNumber * (int)((firstNumber + secondNumber) / thirdNumber));
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/Inventory/Inventory.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            string[] command = Console.ReadLine().Split('-');
+
+            while (command[0].Trim() != "Craft!")
+            {
+                string item = command[1].Trim();
+
+                switch (command[0].Trim())
+                {
+                    case "Collect":
+
+                        if (items.Contains(item) == false)
+                        {
+                            items.Add(item);
+                        }
+
+                        break;
+
+                    case "Drop":
+
+                        if (items.Contains(item))
+                        {
+                            items.Remove(item);
+                        }
+
+                        break;
+
+                    case "Combine Items":
+
+                        string[] itemsToCombine = command[1].Split(':');
+                        string oldItem = itemsToCombine[0].Trim();
+                        string newItem = itemsToCombine[1].Trim();
+
+                        if (items.Contains(oldItem))
+                        {
+                            int index = items.IndexOf(oldItem) + 1;
+                            items.Insert(index, newItem);
+                        }
+
+                        break;
+
+                    case "Renew":
+
+                        if (items.Contains(item))
+                        {
+                            items.Remove(item);
+                            items.Add(item);
+                        }
+
+                        break;
+                }
+
+                command = Console.ReadLine().Split(new string[] { "-" }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            Console.Write(string.Join(", ", items));
+            Console.WriteLine();
+        }
+    }
+}

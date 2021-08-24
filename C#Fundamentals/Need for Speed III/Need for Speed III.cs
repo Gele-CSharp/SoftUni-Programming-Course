2acf3de2eae58965bbdb97ca38804d00231052e2
Gele-CSharp <georgiev.angel@yahoo.com>
2021-08-24 15:05:01 +0300

    Added more solutions.

---
+++ C#Fundamentals/Need for Speed III/Need for Speed III.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Need_for_Speed_III
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+            var carMileage = new Dictionary<string, int>();
+            var carFuel = new Dictionary<string, int>();
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] carInfo = Console.ReadLine().Split(new string[] { "|" }, StringSplitOptions.RemoveEmptyEntries);
+                string car = carInfo[0];
+                int mileage = int.Parse(carInfo[1]);
+                int fuel = int.Parse(carInfo[2]);
+
+                carMileage.Add(car, mileage);
+                carFuel.Add(car, fuel);
+            }
+
+            string[] command = Console.ReadLine().Split(new string[] { ":" }, StringSplitOptions.RemoveEmptyEntries);
+
+            
+            while (command[0] != "Stop")
+            {
+                string car = command[1].Trim();
+
+                if (command[0].Trim() == "Drive")
+                {
+                    int distance = int.Parse(command[2]);
+                    int fuel = int.Parse(command[3]);
+
+                    if (carFuel[car] >= fuel)
+                    {
+                        carFuel[car] -= fuel;
+                        carMileage[car] += distance;
+                        Console.WriteLine($"{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.");
+
+                        if (carMileage[car] >= 100000)
+                        {
+                            carFuel.Remove(car);
+                            carMileage.Remove(car);
+
+                            Console.WriteLine($"Time to sell the {car}!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not enough fuel to make that ride");
+                    }
+                }
+                else if (command[0].Trim() == "Refuel")
+                {
+                    int fuel = int.Parse(command[2]);
+
+                    if (carFuel[car] + fuel > 75)
+                    {
+                        fuel = 75 - carFuel[car];
+                    }
+
+                    carFuel[car] += fuel;
+
+                    Console.WriteLine($"{car} refueled with {fuel} liters");
+                }
+                else if (command[0].Trim() == "Revert")
+                {
+                    int distance = int.Parse(command[2]);
+
+                    if (carMileage[car] - distance >= 0)
+                    {
+                        carMileage[car] -= distance;
+                    }
+
+                    if (carMileage[car] < 10000)
+                    {
+                        carMileage[car] = 10000;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{car} mileage decreased by {distance} kilometers");
+                    }
+                }
+
+                command = Console.ReadLine().Split(new string[] { ":" }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var item in carMileage.OrderByDescending(c=>c.Value))
+            {
+                foreach (var car in carFuel.OrderBy(c=>c.Key).Where(c=>c.Key == item.Key))
+                {
+                    Console.WriteLine($"{car.Key} -> Mileage: {item.Value} kms, Fuel in the tank: {car.Value} lt.");
+                }
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/Nether Realms/Nether Realms.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Nether_Realms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, string> demons = new SortedDictionary<string, string>();
+
+            string input = Console.ReadLine();
+            string namePattern = @"([--}]{0,}[!-+]{0,}){1,}";
+
+            MatchCollection demonNames = Regex.Matches(input, namePattern);
+
+            foreach (Capture demonName in demonNames)
+            {
+                if (demonName.Value.Length > 0)
+                {
+                    string damagePattern = @"(-{0,1}\+{0,1}[0-9]+\.{0,1}[0-9]*)";
+
+                    MatchCollection damage = Regex.Matches(demonName.Value, damagePattern);
+
+                    double damageValue = 0;
+                    int healthValue = 0;
+
+                    foreach (Capture value in damage)
+                    {
+                        damageValue += Convert.ToDouble(value.Value);
+                    }
+
+                    if (demonName.Value.Contains("*"))
+                    {
+                        string name = demonName.Value.ToString();
+                        int multiplier = name.Length;
+                        name = name.Replace("*", "");
+                        multiplier -= name.Length;
+                        damageValue *= multiplier * 2;
+                    }
+                    else if (demonName.Value.Contains("/"))
+                    {
+                        string name = demonName.Value.ToString();
+                        int divider = name.Length;
+                        name = name.Replace("/", "");
+                        divider -= name.Length;
+                        damageValue /= divider * 2;
+                    }
+
+                    string healthPattern = @"([^+-/\*0123456789\.])";
+
+                    MatchCollection health = Regex.Matches(demonName.Value, healthPattern);
+
+                    foreach (Capture value in health)
+                    {
+                        healthValue += Convert.ToInt32(Convert.ToChar(value.Value));
+                    }
+
+                    string status = $"{healthValue} health, {damageValue:f2} damage";
+
+                    demons.Add(demonName.Value.ToString(), status);
+                }
+            }
+
+            foreach (var demon in demons)
+            {
+                Console.WriteLine($"{demon.Key} - {demon.Value}");
+            }
+        }
+    }
+}

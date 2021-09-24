52dad88fc428e7e2e6c4d6d05c7b4f45c45e1660
Gele-CSharp <georgiev.angel@yahoo.com>
2021-09-24 13:18:40 +0300

    Added new solutions.

---
+++ C#Advanced/Word Count 2/Word Count 2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Word_Count_2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string[] words = await File.ReadAllLinesAsync("words.txt");
+            string[] lines = await File.ReadAllLinesAsync("text.txt");
+            Dictionary<string, int> countOfWords = new Dictionary<string, int>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < words.Length; j++)
+                {
+                    string currentWord = words[j].ToLower();
+
+                    if (lines[i].ToLower().Contains(currentWord))
+                    {
+                        if(countOfWords.ContainsKey(currentWord) == false)
+                        {
+                            countOfWords.Add(currentWord, 0);
+                        }
+
+                        countOfWords[currentWord]++;
+                    }
+                }
+            }
+
+            //foreach (var word in countOfWords.OrderByDescending(c => c.Value))
+            //{
+            //    Console.WriteLine(word.Key);
+            //}
+
+            countOfWords.OrderByDescending(c => c.Value);
+            await File.WriteAllLinesAsync("output.txt", countOfWords.OrderByDescending(c=>c.Value).Select(c => c.Key + "-" + c.Value));
+        }
+    }
+}
---
+++ C#Advanced/Word Count/Word Count.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Word_Count
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            using (StreamReader streamOfWantedWords = new StreamReader("words.txt"))
+            {
+                string line = await streamOfWantedWords.ReadLineAsync();
+                string[] wantedWords = line.Split();
+
+                for (int i = 0; i < wantedWords.Length; i++)
+                {
+                    words.Add(wantedWords[i].ToLower(), 0);
+                }
+            }
+
+            using (StreamReader streamOfInput = new StreamReader("Input.txt"))
+            {
+                string line = await streamOfInput.ReadLineAsync();
+                
+                while (line != null)
+                {
+                    string separator = @"[^a-zA-Z+\'*a-z+]";
+                    string[] input = Regex.Split(line, separator);
+
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        string currentWord = input[i].ToLower();
+
+                        if (words.ContainsKey(currentWord))
+                        {
+                            words[currentWord]++;
+                        }
+                    }
+                    line = await streamOfInput.ReadLineAsync();
+                }
+            }
+
+            using (StreamWriter streamOutput = new StreamWriter("Output.txt"))
+            {
+                foreach (var word in words.OrderByDescending(w => w.Value))
+                {
+                    await streamOutput.WriteLineAsync($"{word.Key} - {word.Value}");
+                }
+            }
+        }
+    }
+}

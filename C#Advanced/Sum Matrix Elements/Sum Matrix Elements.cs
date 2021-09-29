61a8698c1737abe5972b8b4f3c1dba10acf470ae
Gele-CSharp <georgiev.angel@yahoo.com>
2021-09-29 18:28:17 +0300

    Added new solutions.

---
+++ C#Advanced/Sum Matrix Elements/Sum Matrix Elements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int numberOfRows = matrixSize[0];
+            int numberOfCols = matrixSize[1];
+            int[,] matrix = new int[numberOfRows, numberOfCols];
+            int sum = 0;
+
+            for (int row = 0; row < numberOfRows; row++)
+            {
+                int[] elements = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < numberOfCols; col++)
+                {
+                    matrix[row, col] = elements[col];
+                    sum += elements[col];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ C#Advanced/The Party Reservation Filter Module/Predicate Party! V3.1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Predicate_Party_V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> people = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            Func<string, string, bool> nameStartString
+                = (name, certainString) => name.Substring(0, certainString.Length) != certainString;
+
+            Func<string, string, bool> nameEndString
+                = (name, certainString) => name.Substring((name.Length - certainString.Length), certainString.Length) != certainString;
+
+            Func<string, int, bool> nameLength = (name, length) => name.Length == length;
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                if (command[0] == "Party!")
+                {
+                    break;
+                }
+
+                if (command[0] == "Remove")
+                {
+                    string certainString = command[2];
+
+                    if (command[1] == "StartsWith")
+                    {
+                        people = people.Where(p => nameStartString(p, certainString)).ToList();
+                    }
+                    else if (command[1] == "EndsWith")
+                    {
+                        people = people.Where(p => nameEndString(p, certainString)).ToList();
+                    }
+                    else if (command[1] == "Length")
+                    {
+                        int length = int.Parse(command[2]);
+                        people = people.Where(p => nameLength(p, length)).ToList();
+                    }
+                }
+                else if (command[0] == "Double")
+                {
+                    string certainString = command[2];
+                    List<string> peopleToDouble = new List<string>();
+                    List<string> doubledListOfPeople = new List<string>();
+
+                    if (command[1] == "StartsWith")
+                    {
+                        peopleToDouble = people.Where(p => !nameStartString(p, certainString)).ToList();
+                    }
+                    else if (command[1] == "EndsWith")
+                    {
+                        peopleToDouble = people.Where(p => !nameEndString(p, certainString)).ToList();
+                    }
+                    else if (command[1] == "Length")
+                    {
+                        int length = int.Parse(command[2]);
+                        peopleToDouble = people.Where(p => nameLength(p, length)).ToList();
+                    }
+
+                    foreach (var person in people)
+                    {
+                        if (peopleToDouble.Contains(person))
+                        {
+                            doubledListOfPeople.Add(person);
+                        }
+
+                        doubledListOfPeople.Add(person);
+                    }
+
+                    if (doubledListOfPeople.Count > 0)
+                    {
+                        people = doubledListOfPeople;
+                    }
+                }
+            }
+
+            if (people.Count > 0)
+            {
+                Console.Write($"{string.Join(", ", people)} are going to the party!");
+            }
+            else
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+        }
+    }
+}

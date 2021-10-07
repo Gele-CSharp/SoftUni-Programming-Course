683466dbc9d57c53d345bd9e2de160fac19158e9
Gele-CSharp <georgiev.angel@yahoo.com>
2021-10-07 13:17:34 +0300

    Added two solutions.

---
+++ C#Advanced/Masterchef2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Masterchef2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> ingredientValues = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            Stack<int> freshnessLevel = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            List<Dish> dishes = new List<Dish>()
+            {
+                new Dish("Dipping sauce", 150),
+                new Dish("Green salad", 250),
+                new Dish("Chocolate cake", 300),
+                new Dish("Lobster", 400)
+            };
+
+            while (ingredientValues.Count > 0 && freshnessLevel.Count > 0)
+            {
+                if(ingredientValues.Peek() == 0)
+                {
+                    ingredientValues.Dequeue();
+                    continue;
+                }
+
+                int neededFreshnessLevel = ingredientValues.Peek() * freshnessLevel.Pop();
+                bool isDishMade = false;
+
+                for (int i = 0; i < dishes.Count; i++)
+                {
+                    if (dishes[i].Freshness == neededFreshnessLevel)
+                    {
+                        dishes[i].Count++;
+                        ingredientValues.Dequeue();
+                        isDishMade = true;
+                        break;
+                    }
+                }
+
+                if (isDishMade == false)
+                {
+                    ingredientValues.Enqueue(ingredientValues.Dequeue() + 5);
+                }
+            }
+
+            List<Dish> output = dishes.OrderBy(d=> d.Name).Where(d => d.Count > 0).ToList();
+
+            if (output.Count == 4)
+            {
+                Console.WriteLine("Applause! The judges are fascinated by your dishes!");
+            }
+            else
+            {
+                Console.WriteLine("You were voted off. Better luck next year.");
+            }
+
+            if (ingredientValues.Count > 0)
+            {
+                Console.WriteLine($"Ingredients left: {ingredientValues.Sum()}");
+            }
+
+            foreach (var dish in output)
+            {
+                Console.WriteLine($" # {dish.Name} --> {dish.Count}");
+            }
+        }
+    }
+}
+
+public class Dish
+{
+    public Dish(string name, int freshness)
+    {
+        Name = name;
+        Freshness = freshness;
+    }
+
+    public string Name { get; set; }
+    public int Freshness { get; set; }
+    public int Count { get; set; }
+}
---
+++ C#Advanced/Survivor/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Survivor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfRows = int.Parse(Console.ReadLine());
+            string[][] beach = new string[numberOfRows][];
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                string[] rowInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                beach[i] = rowInfo;
+            }
+
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Player player = new Player();
+            Player opponent = new Player();
+
+            while (command[0] != "Gong")
+            {
+                int row = int.Parse(command[1]);
+                int col = int.Parse(command[2]);
+
+                if (command[0] == "Find")
+                {
+                    //if (row >= 0 && row < numberOfRows && col >= 0 && col < beach[row].Length)
+                    //{
+                    //    if (beach[row][col] == "T")
+                    //    {
+                    //        collectedTokens++;
+                    //        beach[row][col] = "-";
+                    //    }
+                    //}
+                    //else if (command[0] == "Opponent")
+                    //{
+                    //    string direction = command[3];
+
+                    //    if(row >= 0 && row < numberOfRows && col >= 0 && col < beach[row].Length)
+                    //    {
+                    //        if ()
+                    //    }
+                    //}
+
+                    player.CollectToken(beach, row, col);
+                }
+                else if (command[0] == "Opponent")
+                {
+                    string direction = command[3];
+
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        opponent.CollectToken(beach, row, col);
+
+                        switch (direction)
+                        {
+                            case "up": row--;
+                                break;
+                            case "down": row++;
+                                break;
+                            case "right": col++;
+                                break;
+                            case "left": col--;
+                                break;
+                        }
+                    }
+                }
+
+                command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            for (int row = 0; row < numberOfRows; row++)
+            {
+                for (int col = 0; col < beach[row].Length; col++)
+                {
+                    Console.Write(beach[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Collected tokens: {player.NumberOfTokens}");
+            Console.WriteLine($"Opponent's tokens: {opponent.NumberOfTokens}");
+        }
+    }
+
+    public class Player
+    {
+        public int NumberOfTokens { get; set; }
+
+        public  void CollectToken(string[][] field, int row, int col)
+        {
+            if (row >= 0 && row < field.GetLength(0) && col >= 0 && col < field[row].Length)
+            {
+                if (field[row][col] == "T")
+                {
+                    this.NumberOfTokens++;
+                    field[row][col] = "-";
+                }
+            }
+        }
+    }
+}

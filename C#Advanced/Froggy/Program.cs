e7c800c33f686c5bd6a491431ef88f4c50c25fc5
Gele-CSharp <georgiev.angel@yahoo.com>
2021-10-16 23:32:55 +0300

    Added new solution.

---
+++ C#Advanced/Froggy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lake lake = new Lake(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            List<int> output = new List<int>();
+
+            foreach (var stone in lake)
+            {
+                output.Add(stone);
+            }
+
+            Console.WriteLine(string.Join(", ", output));
+        }
+    }
+}

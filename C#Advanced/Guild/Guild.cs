1d4df254a5e23d32850bbd343a1a2467f45f02ad
Gele-CSharp <georgiev.angel@yahoo.com>
2021-10-20 11:15:40 +0300

    Added solutions.

---
+++ C#Advanced/Guild/Guild.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        private List<Player> players;
+
+        public Guild(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            players = new List<Player>();
+        }
+
+        public string Name { get; private set; }
+
+        public int Capacity { get; private set; }
+
+        public int Count { get; private set; }
+
+        public void AddPlayer(Player player)
+        {
+            if (players.Count < Capacity)
+            {
+                players.Add(player);
+                Count++;
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            bool isRemoved = false;
+
+            if (players.Select(p=> p.Name).Contains(name))
+            {
+                Player playerToRemove = players.Where(p => p.Name == name).FirstOrDefault();
+                players.Remove(playerToRemove);
+                Count--;
+                isRemoved = true;
+            }
+
+            return isRemoved;
+        }
+
+        public void PromotePlayer(string name)
+        {
+            if (players.Select(p=> p.Name).Contains(name))
+            {
+                Player playerToPromote = players.Where(p => p.Name == name).FirstOrDefault();
+                playerToPromote.Rank = "Member";
+            }
+        }
+
+        public void DemotePlayer(string name)
+        {
+            if (players.Select(p => p.Name).Contains(name))
+            {
+                Player playerToDemote = players.Where(p => p.Name == name).FirstOrDefault();
+                playerToDemote.Rank = "Trial";
+            }
+        }
+
+        public Player[] KickPlayersByClass(string @class)
+        {
+            Player[] kickedPlayers = players.Where(p => p.Class == @class).ToArray();
+            players = players.Where(p => p.Class != @class).ToList();
+            Count -= kickedPlayers.Length;
+            return kickedPlayers;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Players in the guild: {Name}");
+
+            foreach (var player in players)
+            {
+                sb.AppendLine(player.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ C#Advanced/Openning/Bakery.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private List<Employee> employees;
+
+        public Bakery(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            employees = new List<Employee>();
+        }
+
+        public string Name { get; private set; }
+
+        public int Capacity { get; private set; }
+
+        public int Count { get; private set; }
+
+        public void Add(Employee employee)
+        {
+            if (employees.Count < Capacity)
+            {
+                employees.Add(employee);
+                Count++;
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            bool isRemoved = false;
+
+            if (employees.Select(e=> e.Name).Contains(name))
+            {
+                Employee employeeToRemove = employees.Where(e => e.Name == name).FirstOrDefault();
+                employees.Remove(employeeToRemove);
+                Count--;
+                isRemoved = true;
+            }
+
+            return isRemoved;
+        }
+
+        public Employee GetOldestEmployee()
+        {
+            Employee oldestEmployee = employees.OrderByDescending(e => e.Age).FirstOrDefault();
+            return oldestEmployee;
+        }
+
+        public Employee GetEmployee(string name)
+        {
+            if (employees.Select(e => e.Name).Contains(name))
+            {
+                Employee employee = employees.Where(e => e.Name == name).FirstOrDefault();
+                return employee;
+            }
+
+            return null;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Employees working at Bakery {Name}:");
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine(employee.ToString());
+            }
+
+            return sb.ToString();
+        }
+    }
+}

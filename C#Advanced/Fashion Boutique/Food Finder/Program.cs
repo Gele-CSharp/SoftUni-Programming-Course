0fa97f6ad31ec2abf8c864aed50c73afbcbe9cf2
Gele-CSharp <georgiev.angel@yahoo.com>
2021-10-25 21:04:10 +0300

    Added two solutions.

---
+++ C#Advanced/Fashion Boutique/Food Finder/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Food_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> vowels = new Queue<string>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries));
+            Stack<string> consonats = new Stack<string>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries));
+            Dictionary<string, string> words = new Dictionary<string, string>()
+            {
+                { "pear", "" },
+                { "flour", "" },
+                { "pork", "" },
+                { "olive", "" }
+            };
+
+            List<string> listOfWords = new List<string>() { "pear", "flour", "pork", "olive" };
+
+            while(consonats.Count > 0)
+            {
+                for (int i = 0; i < listOfWords.Count; i++)
+                {
+                    if (listOfWords[i].Contains(vowels.Peek()) && words[listOfWords[i]].Contains(vowels.Peek()) == false)
+                    {
+                        words[listOfWords[i]] += vowels.Peek();
+                    }
+
+                    if (listOfWords[i].Contains(consonats.Peek()) && words[listOfWords[i]].Contains(consonats.Peek()) == false)
+                    {
+                        words[listOfWords[i]] += consonats.Peek();
+                    }
+                }
+
+                vowels.Enqueue(vowels.Dequeue());
+                consonats.Pop();
+            }
+
+            Dictionary<string, string> result = words.Where(w => w.Value.Length == w.Key.Length).ToDictionary(w => w.Key, w => w.Value);
+
+            Console.WriteLine($"Words found: {result.Count}");
+
+            foreach (var word in result)
+            {
+                Console.WriteLine(word.Key);
+            }
+        }
+    }
+}
---
+++ C#Advanced/Pawn Wars/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+
+namespace Pawn_Wars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] chessBoard = new string[8, 8];
+            int whitePawnRow = -1;
+            int whitePawnCol = -1;
+            int blackPawnRow = -1;
+            int blackPawnCol = -1;
+
+            for (int r = 0; r < 8; r++)
+            {
+                string rowInfo = Console.ReadLine();
+
+                for (int c = 0; c < 8; c++)
+                {
+                    chessBoard[r, c] = rowInfo[c].ToString();
+
+                    if (chessBoard[r, c] == "w")
+                    {
+                        whitePawnRow = r;
+                        whitePawnCol = c;
+                    }
+                    else if (chessBoard[r, c] == "b")
+                    {
+                        blackPawnRow = r;
+                        blackPawnCol = c;
+                    }
+                }
+            }
+
+
+            int numberOfMove = 0;
+
+            while (true)
+            {
+                if (numberOfMove % 2 == 0)
+                {
+                    chessBoard[whitePawnRow, whitePawnCol] = "-";
+                    whitePawnRow--;
+
+                    if (whitePawnRow > 0)
+                    {
+                        if (IsThereCapture(chessBoard, whitePawnRow, whitePawnCol))
+                        {
+                            Console.WriteLine($"Game over! White capture on {Convert.ToChar(blackPawnCol + 97)}{8 - blackPawnRow}.");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Game over! White pawn is promoted to a queen at {Convert.ToChar(whitePawnCol + 97)}{8 - whitePawnRow}.");
+                        return;
+                    }
+
+                    chessBoard[whitePawnRow, whitePawnCol] = "w";
+
+                }
+                else
+                {
+                    chessBoard[blackPawnRow, blackPawnCol] = "-";
+                    blackPawnRow++;
+
+                    if (blackPawnRow < 7)
+                    {
+                        if (IsThereCapture(chessBoard, blackPawnRow, blackPawnCol))
+                        {
+                            Console.WriteLine($"Game over! Black capture on {Convert.ToChar(whitePawnCol + 97)}{8 - whitePawnRow}.");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Game over! Black pawn is promoted to a queen at {Convert.ToChar(blackPawnCol + 97)}{8 - blackPawnRow}.");
+                        return;
+                    }
+
+                    chessBoard[blackPawnRow, blackPawnCol] = "b";
+                }
+
+                numberOfMove++;
+            }
+        }
+
+        public static bool IsThereCapture(string[,] chessBoard, int row, int col)
+        {
+            if (col + 1 <= 7)
+            {
+                if (chessBoard[row, col + 1] == "b" || chessBoard[row, col + 1] == "w")
+                {
+                    return true;
+                }
+            }
+
+            if (col - 1 >= 0)
+            {
+                if (chessBoard[row, col - 1] == "w" || chessBoard[row, col - 1] == "b")
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}

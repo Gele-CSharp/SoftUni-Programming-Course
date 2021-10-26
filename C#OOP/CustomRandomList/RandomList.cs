749e06926ceb0d59d4b0c2f2f00d5f740f423d18
Gele-CSharp <georgiev.angel@yahoo.com>
2021-10-26 18:47:11 +0300

    Added new solutions.

---
+++ C#OOP/CustomRandomList/RandomList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class RandomList: List<string>
+    {
+        private List<string> items;
+
+        public RandomList(params string[] items)
+        {
+            this.items = new List<string>(items);
+        }
+
+        public string RandomString()
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(0, this.Count - 1);
+            string randomString = this[index];
+            this.RemoveAt(index);
+            return randomString;
+        }
+    }
+}
---
+++ C#OOP/CustomRandomList/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList()
+            {
+                "Pesho",
+                "Gosho",
+                "Ivan",
+                "Petkan",
+                "Dragan"
+            };
+
+            Console.WriteLine(randomList.RandomString());
+        }
+    }
+}

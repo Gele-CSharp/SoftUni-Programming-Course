e0e340779880747362762714c9c6798578497103
Gele-CSharp <georgiev.angel@yahoo.com>
2021-11-18 20:52:06 +0200

    Added new solution.

---
+++ C#OOP/BarcodeGenerator/BarcodeGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BarcodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3546;
+
+            Console.WriteLine((n / 10) % 10);
+
+
+
+        }
+    }
+}

5327f304c18f4f3257ec3da94ba29f104e4da774
Gele-CSharp <georgiev.angel@yahoo.com>
2021-11-19 12:14:35 +0200

    Added few solutions

---
+++ C#Advanced/Equal Sums Even Odd Position 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Equal_Sums_Even_Odd_Position_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = num1; i <= num2; i++)
+            {
+                string currentNum = i.ToString();
+                oddSum = 0;
+                evenSum = 0;
+
+                for (int j = 0; j < currentNum.Length; j++)
+                {
+                    int currentDigit = int.Parse(currentNum[j].ToString());
+
+                    if (j % 2 == 0)
+                    {
+                        evenSum += currentDigit;
+                    }
+                    else
+                    {
+                        oddSum += currentDigit;
+                    }
+                }
+                if (evenSum == oddSum)
+                {
+                    Console.Write($"{i} ");
+                }
+
+            }
+        }
+    }
+}
---
+++ C#Advanced/Equal Sums Even Odd Position/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Equal_Sums_Even_Odd_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int oddSum = 0;
+            int evenSum = 0;
+            int counter = 0;
+            int digit = 0;
+
+            for (int i = num1; i <= num2; i++)
+            {
+                int currentNumber = i;
+
+                while (counter < 6)
+                {
+                    
+                    digit = currentNumber % 10;
+
+                    if (counter % 2 == 0)
+                    {
+                        evenSum += digit;
+                    }
+                    else
+                    {
+                        oddSum += digit;
+                    }
+
+                    currentNumber /= 10;
+                    counter++;
+                }
+                if (evenSum == oddSum)
+                {
+                    Console.Write($"{i} ");
+                }
+                counter = 0;
+                evenSum = 0;
+                oddSum = 0;
+            }
+
+        }
+    }
+}
